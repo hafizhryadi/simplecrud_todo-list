8d3bfd192b4b884536cfd0046b360b2ccc2f0226
hafizhryadi <hafizhryadi@gmail.com>
2022-12-03 14:58:00 +0700

    first commit

---
+++ src/components/Header.js
@@ -0,0 +1,37 @@
+import React from 'react'
+import { useState } from 'react'
+
+const Header = ({setRefresh}) => {
+    const [title, setTitle] = useState("");
+
+    const addTodo = () => {
+        const newTodo = {title, done: false}
+
+        fetch('http://localhost:6969/todos', {
+            method: 'POST',
+            headers: {
+                "Content-Type": "application/json"
+            },
+            body: JSON.stringify(newTodo)
+        }).then(() => {
+            setTitle("")
+
+            setRefresh(true)
+
+            setTimeout(() => {
+                alert('new todo added.')
+            }, 500)
+        });
+    }
+    
+  return (
+    <div id='todo-header' className='header'>
+        <h2>Simple Todo App</h2>
+        <input type="text" value={title} onChange={(e) => setTitle(e.target.value)} />
+        
+        <span className='add-button' onClick={addTodo}>Add</span>
+    </div>
+  )
+}
+
+export default Header
